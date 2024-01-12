58b671ea51b90d738dc1ccc8bcf8fac7abf12df2
ThithipongRuesupa <Rockqr.rocker@gmail.com>
2024-01-13 00:57:37 +0700

    Commit Check and update package

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <v-app>
-    <v-app-bar color="#DDDDDD" dense dark>
+  <v-app v>
+    <v-app-bar color="green" dense dark>
       <v-spacer></v-spacer> <v-spacer></v-spacer>
-      <v-btn variant="flat" color="EEEEEE" rounded="pill">
+      <v-btn variant="flat" elevation="24" color="" rounded="pill">
         <RouterLink to="/">หน้าแรก</RouterLink></v-btn
       > <v-spacer></v-spacer>
-      <v-btn variant="flat" small elevation="" color="" rounded="pill">
+      <v-btn variant="flat" elevation="24" color="" rounded="pill">
         <RouterLink to="/courses">รายวิชา</RouterLink></v-btn
       > <v-spacer></v-spacer>
-      <v-btn  variant="flat" small elevation="" color="" rounded="pill">
+      <v-btn  variant="flat" elevation="24" color="" rounded="pill">
         <RouterLink to="/enrollments">ลงทะเบียนเรียน</RouterLink></v-btn
       > <v-spacer></v-spacer>
-      <v-btn variant="flat" small elevation="" color="" rounded="pill">
+      <v-btn variant="flat" elevation="24" color="" rounded="pill">
         <RouterLink to="/mycourse">วิชาที่ฉันลงทะเบียน</RouterLink></v-btn
       ><v-spacer></v-spacer> <v-spacer></v-spacer>
     </v-app-bar>
